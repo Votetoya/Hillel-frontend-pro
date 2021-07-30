dec0c0f55bce0f1a51e6d22278004c5d219498af
Votetoya <votetoyametra@gmail.com>
2021-07-30 20:31:25 +0300

    first commit

---
+++ first/js/custom.js
@@ -0,0 +1,18 @@
+var h ='Hello ';
+var w ='World';
+
+var result = h + w
+
+console.log(h + w)
+
+var someArr =['Alex', 2342342, false, ['test'], 23453453];
+
+console.log('someArr ----->', someArr);
+
+var userInfo = {
+    userName: 'Alex',
+    age: 30,
+    gender: 'male'
+}
+
+console.log('userInfo.userName ----------->', userInfo.userName, userInfo.age)
