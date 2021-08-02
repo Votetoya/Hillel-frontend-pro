1e8d58471a54728c77a0711fd436481bd265c77a
Votetoya <votetoyametra@gmail.com>
2021-08-02 15:33:53 +0300

    change var pi

--- lesson01/js/custom.js
+++ lesson01/js/custom.js
@@ -1,9 +1,12 @@
 var h = 5;
 var r = 2;
 var r2 = r * r;
+var Math.PI = 3.14159
 
 var result = 3.14159 * r2 * h
 
 console.log(3.14159 * r2 * h)
 
 console.log(result)
+
+console.log(Math.PI * r2 * h)
