74b020355d14eeaa0bbc65268b684d6eb486f689
Votetoya <votetoyametra@gmail.com>
2021-08-05 10:01:20 +0300

    HW 2

--- lesson01/js/custom.js
+++ lesson01/js/custom.js
@@ -1,12 +1,2 @@
-var h = 5;
-var r = 2;
-var r2 = r * r;
-
-
-var result = 3.14159 * r2 * h
-
-console.log(3.14159 * r2 * h)
-
-console.log(result)
-
-console.log(Math.PI * r2 * h)
+var isAdmin = confirm("Вы - администратор?");
+alert( isAdmin );
---
+++ lesson2/js/custom.js
@@ -0,0 +1,9 @@
+var firstValue = prompt('Ввведите первое значение');
+var secondValue = prompt('Введите второе значение');
+
+console.log(firstValue);
+console.log(secondValue);
+
+if (firstValue > secondValue) alert('firstValue > secondValue')
+else if (firstValue < secondValue) alert('firstValue < secondValue')
+else if (firstValue == secondValue) alert('firstValue = secondValue')
