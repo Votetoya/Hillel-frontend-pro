6d99189f2633ea309368d12e308db5928747e72f
Votetoya <votetoyametra@gmail.com>
2021-08-20 21:26:28 +0300

    HW_8

--- lesson7/js/custom.js
+++ lesson7/js/custom.js
@@ -15,23 +15,18 @@ console.log('--/--/--/--/--/');
 function f(n) {
     return n ? n * f(n-1) : 1
 }
-
 console.log(f(5) );
 */
 
 /*
 var first = 'Hello';
-
 function f(n) {
     return 20
 }
-
 function test(n) {
     return 2012e12312
 }
-
 console.log( f(5) );
-
 test();
 */
 
@@ -39,25 +34,19 @@ test();
 var multiply = function someFunc(a, b) {
     return a * b;
 }
-
 console.log('multiply ->', multiply(2,2));
-
 var multiply2 = (a, b) => a * b;
 var multiply2 = a => a * a;
 var multiply2 = (a, b) => {
     return a * b;
 }
-
 console.log('multiply2 ->', multiply2(2,2));
-
 */
 /*
-
 for (var i = 2; i < 11; i += 2) {
     
     console.log(i);
  }
-
  */
 
  /*
@@ -66,16 +55,13 @@ for (var i = 2; i < 11; i += 2) {
      if (i % 2) 
      console.log(i);
  }
-
  console.log(i);
  */
 
  /*
 var userNames = ['Alex', 'Nastya', 'Vasya', 'Olga', 'Max', 'Sergey'];
-
 console.log(userNames.length) // 6
 console.log(userNames) 
-
 for (var i = 0; i < userNames.length; i++) {
     console.log('Hello ' + userNames[i]);
 }
