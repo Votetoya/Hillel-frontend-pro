dcf80bc1fd7a191f89eef42b9df329b58d9c1837
Votetoya <votetoyametra@gmail.com>
2021-09-05 19:52:19 +0300

    HW13 completed

--- lesson13/custom.js
+++ lesson13/custom.js
@@ -1,2 +1,14 @@
-const button= document.querySelector('button');
-const input= document.querySelector('input');
+const ul = document.createElement('ul');
+const but = document.querySelector('button');
+const inp = document.querySelector('[name="AnyInput"]'); 
+
+but.addEventListener('click', () => {
+    if(inp.value !== '') {
+    document.body.prepend(ul);
+    let list = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerText = inp.value;
+    list.appendChild(li);
+    inp.value = null
+    }
+})
