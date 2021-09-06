08df2cd9ba5a5bf64c60431c9486130c28b45003
Votetoya <votetoyametra@gmail.com>
2021-09-06 19:24:51 +0300

    Begin working HW14

--- lesson2/js/custom.js
+++
@@ -1,9 +0,0 @@
-var firstValue = prompt('Enter the first value');
-var secondValue = prompt('Enter the second value');
-
-console.log(firstValue);
-console.log(secondValue);
-
-if (firstValue > secondValue) alert('firstValue > secondValue')
-else if (firstValue < secondValue) alert('firstValue < secondValue')
-else if (firstValue === secondValue) alert('firstValue = secondValue')
