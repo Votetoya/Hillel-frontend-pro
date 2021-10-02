732fe50416aadfb166be9a1c8fce84eaeb2e91ee
Votetoya <votetoyametra@gmail.com>
2021-10-02 14:05:58 +0300

    master update

--- lesson17/custom.js
+++ lesson17/custom.js
@@ -1,1 +1,41 @@
-'use strict'
+'use strict';
+
+    let num = 100; 
+
+   const calc = {
+    add: function (n) {
+        if (typeof n === 'number')
+        {
+        console.log(num += n);
+        }
+        else 
+        {
+          console.log('NaN');
+        }
+    },
+    sub: function (n) {
+        if (typeof n === 'number')
+          {
+          console.log(num = num - n);
+          }
+          else 
+          {
+            console.log('NaN');
+          } 
+    }, 
+    set: function (n) {
+        if (typeof n === 'number')
+          {
+          console.log(num = n);
+          }
+          else 
+          {
+            console.log('NaN');
+          } 
+    },
+    get: function (n) {
+        document.write(num + '<br>');
+        console.log(num);
+    }
+}
+   
