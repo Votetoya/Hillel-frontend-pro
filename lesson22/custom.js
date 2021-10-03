f24186cab7aeed50d8ef02b7bf08e9c50a399f21
Votetoya <votetoyametra@gmail.com>
2021-10-03 18:09:29 +0300

    HW22

--- lesson22/custom.js
+++ lesson22/custom.js
@@ -1,20 +1,3 @@
-/*  fetch('https://api.github.com/users/Votetoya')
-   .then((resp) => resp.json())
-   .then((data) => console.log(data.avatar_url, ' - ', data.node_id))  */
-
-
-
-/* fetch('https://api.github.com/users/Votetoya');
-
-promise.then((resp) => {
-   const json = resp.json();
-   return json;
-})
-.then((data) => {
-   console.log(data);
-   return data;
-}) */
-
 const buttonGet = document.querySelector('#getUserData');
 const divcontainer = document.querySelector('#resultInfo');
 const inputfield = document.querySelector('#inputUserData');
@@ -23,39 +6,26 @@ buttonGet.addEventListener('click', onButtonGetClick);
 
 
 function onButtonGetClick() {
-   fetch('https://api.github.com/users/' + inputfield.value)
-   .catch(error => console.log("Error", error.message))
-   .then(resp => resp.json()) 
+    getGitInfo()
+
    .then(userdata => divcontainer.innerHTML =  
       `<img src=${userdata.avatar_url}>
       <div>Number of repositories - ${userdata.public_repos}<div>
       <div>Number of followers - ${userdata.followers}<div>
       <div>Number of observed - ${userdata.following}<div>`)
    //.catch(error => alert(error.message))
-   //.catch(error => console.log("Error", error.message))
-}      
-      
+} 
 
-   /* .then((userdata) => divcontainer.insertAdjacentHTML('afterbegin', 
-      `<img src=${userdata.avatar_url}>
-      <div>Number of repositories - ${userdata.public_repos}<div>
-      <div>Number of followers - ${userdata.followers}<div>
-      <div>Number of observed - ${userdata.following}<div>`)) */
-   //.catch (alert("No such user exists"))
-/* 
-   const qwe = new Promise((resolve, reject) => {
-      fetch('https://api.github.com/users/111')
+function getGitInfo() {
   
-      resolve();
-  });
-
-  function onButtonGetClick() {
-   qwe
-   .then((resp) => resp.json())
-   .then((userdata) => divcontainer.innerHTML =  
-      `<img src=${userdata.avatar_url}>
-      <div>Number of repositories - ${userdata.public_repos}<div>
-      <div>Number of followers - ${userdata.followers}<div>
-      <div>Number of observed - ${userdata.following}<div>`)
-      //.catch(alert ("не удалось выполнить...")) //выведет сообщение "не удалось выполнить...")
-}   */
+    return fetch('https://api.github.com/users/' + inputfield.value)
+      .then((res) => {
+        if (!res.ok) { throw response }
+        return res.json()
+      })
+      .then((info) => info)
+      .catch( err => {
+        console.log(err)
+        alert('This user does not exist')
+      })
+  }
