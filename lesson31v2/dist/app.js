380e61513a9ba0afe94c7ce19a703d60174bf13e
Votetoya <votetoyametra@gmail.com>
2021-11-07 22:08:02 +0200

    hw31v2

---
+++ lesson31v2/dist/app.js
@@ -0,0 +1,123 @@
+class TypicodeAPI {
+    static URL = 'https://jsonplaceholder.typicode.com/albums';
+    static HEADERS = {
+      'Content-type': 'application/json; charset=UTF-8',
+    };
+  
+    static request(uri, method, data) {
+      return fetch(`${this.URL}${uri}`, {
+        method,
+        headers: this.HEADERS,
+        body: data ? JSON.stringify(data) : undefined,
+      });
+    }  
+  
+    static getList() {
+      return this.request(`?userId=1`, `GET`)
+        .then((res) => {
+          if(res.ok) {
+            return res.json();
+          }
+  
+        throw new Error('Can not fetch albums from API');
+        })
+        .then((data) => data);
+    }
+  }
+const URLPHOTOS = 'https://jsonplaceholder.typicode.com/photos?albumId';
+  
+const SELECTOR = Object.freeze({
+    TEMPLATE_POSTS: '#postsTemplate',
+    TEMPLATE_PHOTOS: '#photosTemplate',
+    DESCR_LIST: '#descr-list',
+    LIST_PHOTO: '#list-photo',
+  });
+
+const CLASS = Object.freeze({
+    ALBUM: 'album-title',
+    ACTIVE: 'active',
+    ALBUM_ID: 'data-id',
+  })
+  
+const PLACEHOLDER = Object.freeze({
+    DATA_ID: '{{album-id}}',
+    TITLE: '{{title}}',
+    PHOTO_URL: '{{photo-url}}',
+    PHOTO_ID: '{{photo-id}}',
+    TITLE_IMG: '{{title-img}}',
+  });
+  
+const TemplatePosts = document.querySelector(SELECTOR.TEMPLATE_POSTS).innerHTML;
+const TemplatePhotos = document.querySelector(SELECTOR.TEMPLATE_PHOTOS).innerHTML;
+const DescrList = document.querySelector(SELECTOR.DESCR_LIST);
+const ListPhoto = document.querySelector(SELECTOR.LIST_PHOTO);
+
+  function init() {
+    TypicodeAPI.getList()
+      .then(addAlbumsList)
+      .catch((e) => alert(e.message))
+  }
+
+  init();
+  
+  function addAlbumsList(gallery) {
+    const htmlAlbums = gallery.map(post => getHtmlPosts(post)).join('');
+  
+    DescrList.innerHTML = htmlAlbums;
+  
+    const firstAlbum = DescrList.firstElementChild;
+  
+    firstAlbum.classList.add(CLASS.ACTIVE);
+    addAlbumPhotos(firstAlbum);
+  }
+  
+  function getHtmlPosts(post) {
+    return TemplatePosts
+      .replace(PLACEHOLDER.TITLE, post.title)
+      .replace(PLACEHOLDER.DATA_ID, post.id)
+  }
+  
+  DescrList.addEventListener('click', onGalleryListClick);
+  
+  function onGalleryListClick(e) {
+    addActiveAlbum(e);
+  }
+  
+  function addActiveAlbum(e) {
+    const album = getAlbum(e.target);
+    const activeEl = findActiveItem();
+  
+    activeEl.classList.remove(CLASS.ACTIVE);
+    album.classList.add(CLASS.ACTIVE);
+    addAlbumPhotos(album);
+  }
+  
+  function getAlbum(target) {
+    return target.closest('.' + CLASS.ALBUM);
+  }
+  
+  function findActiveItem() {
+    return DescrList.querySelector('.' + CLASS.ACTIVE);
+  }
+  
+  function addAlbumPhotos(album) {
+    const albumId = album.getAttribute(CLASS.ALBUM_ID);
+  
+    fetch(`${URLPHOTOS}=${albumId}`)
+    .then((response) => response.json())
+    .then((photos) => photos)
+    .then(addAlbumList)
+  }
+  
+  function addAlbumList(photos) {
+    const htmlPhotos = photos.map(photo => getHtmlPhoto(photo)).join('');
+  
+    ListPhoto.innerHTML = htmlPhotos;
+  }
+  
+  function getHtmlPhoto(photo) {
+    return TemplatePhotos
+      .replace(PLACEHOLDER.PHOTO_URL, photo.thumbnailUrl)
+      .replace(PLACEHOLDER.PHOTO_ID, photo.id)
+      .replace(PLACEHOLDER.TITLE_IMG, photo.title)
+  }
